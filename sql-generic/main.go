8218b881820531e0b84535cdc1cbb56763e28095
rian.digitalent <rian.digitalent@gmail.com>
2020-10-22 17:24:50 +0700

    sql orm

--- sql-generic/main.go
+++ sql-generic/main.go
@@ -7,6 +7,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/riandigitalent/IntroDatabase/sql-generic/config"
+	"github.com/riandigitalent/IntroDatabase/sql-generic/database"
 	"github.com/spf13/viper"
 )
 
@@ -17,13 +18,36 @@ func main() {
 		return
 	}
 
-	_, err = connect(cfg.Database)
+	db, err := connect(cfg.Database)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	//coba insert
+	//database.InsertCustomer(database.Customer{
+	//	FirstName:    "Rian",
+	//	LastName:     "Yunandar",
+	//	NpwpId: "id-1",
+	//	Age:          10,
+	//	CustomerType: "Sultan",
+	//	Street:       "tiuhbalak",
+	//	City:         "Waykanan",
+	//	State:        "Indo",
+	//	ZipCode:      "35122",
+	//	PhoneNumber:  "0812a3232384",
+	//},db)
+
+	//coba tarik data constumer
+	//database.GetCustomers(db)
+
+	//coba hapus databy id
+	//database.DeleteCustomer(1,db)
+
+	//update umur
+	database.UpdateCustomer(28, 2, db)
 }
 
+//fungsi tarik data yml
 func getConfig() (config.Config, error) {
 	viper.AddConfigPath(".")
 	viper.SetConfigType("yml")
@@ -42,6 +66,7 @@ func getConfig() (config.Config, error) {
 	return cfg, nil
 }
 
+//fungsi konek ke db mysql
 func connect(cfg config.Database) (*sql.DB, error) {
 	db, err := sql.Open(cfg.Driver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DbName, cfg.Config))
 	if err != nil {
